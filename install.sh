a19e69ce3c7e71af00294f36ee78116a2c6c556a
FutureAxolotl <future_axolotl@tutanota.com>
2022-07-25 15:24:14 -0400

    added installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+cp .doom.d/config.el ~/.doom.d/
+cp BetterDiscord/themes/OneDark.theme.css ~/.config/BetterDiscord/themes/
+cp picom.conf ~/.config
+cp -r wallpapers/ ~/Pictures/
+cp -r sxhkd ~/.config
+cp -r rofi ~/.config
+cp -r macchina ~/.config
+cp -r fish ~/.config
+cp -r dunst ~/.config
+cp -r polybar ~/.config
+cp -r bspwm ~/.config
+cp -r alacritty ~/.config
